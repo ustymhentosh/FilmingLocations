e7618ccca806149c9cc5644f173c7b8018b07d52
ustymhentosh <116552529+ustymhentosh@users.noreply.github.com>
2023-02-18 21:36:09 +0200

    Adding documentstion where it is possible

--- main/Generate_map/generate_map.py
+++ main/Generate_map/generate_map.py
@@ -4,7 +4,11 @@ import math
 
 
 def find_distance(lat_1, lon_1, lat_2, lon_2):
-    """" Returns distance between points """
+    """
+    Returns distance between points
+    >>> find_distance(12.234, 23.8488, 45.49494, 33.3737)
+    3824085.168902515
+    """
     lon_1, lat_1, lon_2, lat_2 = map(math.radians, [lon_1, lat_1, lon_2, lat_2])
     haversin = lambda x: math.sin(x/2) ** 2
     d = 2 * 6400 * 1000 * math.asin(math.sqrt(\
@@ -14,9 +18,18 @@ def find_distance(lat_1, lon_1, lat_2, lon_2):
 
 
 def find_ten_closest_places(year, latitude, longitude):
-    """ Finds ten closest places """
+    """
+    Finds ten closest places
+    >>> find_ten_closest_places(2001, 44.3733, 54.3933)
+    [('Nekroloq ', (40.3755885, 49.8328009)), ('Mesto ', (48.663894049999996, 45.729389090262536)),\
+ ("Finally I'm Coming Home ", (40.7696272, 44.6736646)),\
+ ('Pierlequin ', (40.1777112, 44.5126233)), ('Die Reise nach Kafiristan ', (39.7675529, 64.4231326)),\
+ ('...Va man dar khoshbakhti-e shirin be donya amadam! ', (35.6892523, 51.3896004)),\
+ ("The Flower's Sanctuary ", (36.2974945, 59.6059232)), ("Visa pour l'oubli ", (48.1012954, 66.7780818)),\
+ ('DR-Explorer: Rusland ', (53.4242184, 58.983136)), ('Qateh-ye natamam ', (35.7131, 47.2656))]
+    """
     dictionary = {}
-    with open(f'Years partition\{year}.txt', 'r') as raw_info:
+    with open(f'.\Years partition\{year}.txt', 'r') as raw_info:
         for line in raw_info:
             dictionary[eval(line[:line.find(')') + 1])] = eval(line.strip()[line.find('['):])
     
@@ -25,7 +38,10 @@ def find_ten_closest_places(year, latitude, longitude):
 
 
 def generate_map(year, latitude, longitude):
-    """ Generates html map """
+    """
+    Generates html map
+    >>> generate_map(1999, 45.229, 11.2234)
+    """
     res = find_ten_closest_places(year, latitude, longitude)
     rradius = sorted([find_distance(latitude, longitude, lat_lon[1][0], lat_lon[1][1]) for lat_lon in res])[-1]
     map = folium.Map(location=[latitude, longitude], zoom_start= 6)
@@ -41,27 +57,29 @@ def generate_map(year, latitude, longitude):
                                 radius= rradius, weight=2, color="#000"))
     map.add_child(fg)
     map.save('Final_map.html')
-    pass
+    return None
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-                    prog = 'generate_map',
-                    description = 'gererates map of movies')
-    parser.add_argument('year',
-                    type = int,
-                    help ='year when movies were filmed')
-    parser.add_argument('latitude',
-                    type = float,
-                    help ='latitude of user`s position')
-    parser.add_argument('longitude',
-                    type = float,
-                    help ='longitude of user`s position')
-    parser.add_argument('path_to_dataset',
-                    type = str,
-                    help ='path to movies locations dataset')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #                 prog = 'generate_map',
+    #                 description = 'gererates map of movies')
+    # parser.add_argument('year',
+    #                 type = int,
+    #                 help ='year when movies were filmed')
+    # parser.add_argument('latitude',
+    #                 type = float,
+    #                 help ='latitude of user`s position')
+    # parser.add_argument('longitude',
+    #                 type = float,
+    #                 help ='longitude of user`s position')
+    # parser.add_argument('path_to_dataset',
+    #                 type = str,
+    #                 help ='path to movies locations dataset')
+    # args = parser.parse_args()
 
-    generate_map(str(args.year), args.latitude , args.longitude)
+    # generate_map(str(args.year), args.latitude , args.longitude)
 
-    
+    if __name__ == '__main__':
+        import doctest
+        print(doctest.testmod())
---
+++ main/Generate_years_partitioon/database_transformation.py
@@ -0,0 +1,131 @@
+import re
+from geopy.geocoders import Nominatim
+from tqdm import tqdm
+import argparse
+
+## execute if you don`t have this module
+# subprocess.call('pip install geopy')
+
+def get_coords(address: str, precise = False) -> None:
+    """
+    Gets coords from adderss
+    >>> get_coords('Lviv, Ukraine', precise = True)
+    (49.841952, 24.0315921)
+    """
+    try:
+        geolocator = Nominatim(user_agent = "Movies location search")
+        location = geolocator.geocode(address.strip(), timeout=None)
+        if precise:
+            while not location:
+                address = address[address.find(',') + 1 :]
+                location = geolocator.geocode(address.strip(), timeout=None)
+        if location:
+            return (location.latitude, location.longitude)
+    except KeyboardInterrupt:
+        return 'Safe stop'
+    except:
+        return None
+
+
+def read_transform_write(database_path: str) -> dict:
+    """
+    Reads database, finds coordinates, and writes to the new file
+    """
+    names = set()
+    count = set()
+    universal_dictionary = {}
+    with open(database_path, 'r') as raw_file:
+        for line in raw_file:
+            try:
+                # extracting names
+                name = line[:line.find('(')]
+
+                # extracting year
+                year_span = re.search(r'\(\d{4}\)', line).span()
+                year = line[year_span[0] + 1: year_span[1] - 1]
+
+                # extractind address
+                address_search = line[year_span[1]:].strip()
+                address_search = re.sub(pattern = r'\([^)]*\)', repl = '', string = address_search)
+                address_search = re.sub(pattern = r'\{[^)]*\}', repl = '', string = address_search).strip()
+
+                # writing to dictionary
+                if name not in names:
+                    if universal_dictionary.get(year):
+                        if universal_dictionary[year].get(address_search):
+                            universal_dictionary[year][address_search].append(name)
+                        else:
+                            universal_dictionary[year][address_search] = [name]
+                    else:
+                        universal_dictionary[year] = {address_search: [name]}
+                    count.add(address_search)
+                names.add(name)
+            except KeyboardInterrupt:
+                break
+            except:
+                continue
+    return universal_dictionary
+
+
+def engage_decoding(universal_dictionary: dict, year = '') -> dict:
+    """
+    Writes by years and modifies dict
+    >>> engage_decoding({2000: {'Lviv, Ukraine': ["point"]}}, 2000)
+    {(49.841952, 24.0315921): ['point']}
+    """
+    memory = {}
+
+    # one year operation
+    one_year_dct = {}
+    if year:
+        with open(f'{year}.txt', 'w') as write_here:
+            for address in tqdm(universal_dictionary[year]):
+                if not memory.get(address):
+                    result = get_coords(address)
+                    memory[address] = result
+                else:
+                    result = memory[address]
+                # catching stop and writing to the file
+                if result == 'Safe stop':
+                    return None
+                if result:
+                    one_year_dct[result] = universal_dictionary[year][address]
+                    write_here.write(f'{result} == {universal_dictionary[year][address]}\n')
+        return one_year_dct
+
+    # all years
+    dream_dictionary = {}
+    for year in tqdm(universal_dictionary.keys()):
+        with open(f'{year}.txt', 'w') as write_here:
+            dream_dictionary[year] = {}
+            for address in tqdm(universal_dictionary[year].keys()):
+                # remembering done things
+                if not memory.get(address):
+                    result = get_coords(address)
+                    memory[address] = result
+                else:
+                    result = memory[address]
+                # catching stop and writing to the file
+                if result == 'Safe stop':
+                    return None
+                if result:
+                    dream_dictionary[year][result] = universal_dictionary[year][address]
+                    write_here.write(f'{result} == {universal_dictionary[year][address]}\n')
+
+    return dream_dictionary
+
+
+if __name__ == '__main__':
+
+    import doctest
+    print(doctest.testmod())
+
+    parser = argparse.ArgumentParser(
+                    prog = 'transforn data',
+                    description = 'transformes data into needed files')
+    parser.add_argument('file',
+                    type = str,
+                    help ='file where to read')
+    args = parser.parse_args()
+
+    engage_decoding(read_transform_write(args.file, 'unique.txt'))
